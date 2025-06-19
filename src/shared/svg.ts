dfe40e60b5979a783a7a55e0cd7e20bb579d83cf
bigyihsuan <bigyihsuan@gmail.com>
2025-06-18 22:23:07 -0400

    add builder for svg path d

--- src/shared/svg.ts
+++ src/shared/svg.ts
@@ -12,89 +12,122 @@ interface Stringable {
     toString(): string;
 }
 
-export class Path {
+export class PathDirective {
+    directives: Stringable[] = [];
+
+    private add(directives: Stringable[]) {
+        this.directives.push(...directives);
+    }
+
+    public build(): string {
+        return this.directives.join(" ");
+    }
+
+    public toString(): string {
+        return this.build();
+    }
+
     // #region Line Commands
 
-    static moveTo(x: number, y: number) {
-        return ["M", x, y];
+    public moveTo(x: number, y: number): PathDirective {
+        this.add(["M", x, y]);
+        return this;
     }
 
-    static moveToRelative(dx: number, dy: number) {
-        return ["m", dx, dy];
+    public moveToRelative(dx: number, dy: number): PathDirective {
+        this.add(["m", dx, dy]);
+        return this;
     }
 
-    static lineTo(x: number, y: number) {
-        return ["L", x, y];
+    public lineTo(x: number, y: number): PathDirective {
+        this.add(["L", x, y]);
+        return this;
     }
 
-    static lineToRelative(dx: number, dy: number) {
-        return ["l", dx, dy];
+    public lineToRelative(dx: number, dy: number): PathDirective {
+        this.add(["l", dx, dy]);
+        return this;
     }
 
-    static horizontal(x: number) {
-        return ["H", x];
+    public horizontal(x: number): PathDirective {
+        this.add(["H", x]);
+        return this;
     }
 
-    static vertical(x: number) {
-        return ["V", x];
+    public vertical(x: number): PathDirective {
+        this.add(["V", x]);
+        return this;
     }
 
-    static horizontalRelative(dx: number) {
-        return ["h", dx];
+    public horizontalRelative(dx: number): PathDirective {
+        this.add(["h", dx]);
+        return this;
     }
 
-    static verticalRelative(dy: number) {
-        return ["v", dy];
+    public verticalRelative(dy: number): PathDirective {
+        this.add(["v", dy]);
+        return this;
     }
 
-    static close() {
-        return ["Z"];
+    public close(): PathDirective {
+        this.add(["Z"]);
+        return this;
     }
     // #endregion Line Commands
 
     // #region Cubic Bezier
-    static cubic(x1: number, y1: number, x2: number, y2: number, x: number, y: number) {
-        return ["C", x1, y1, ",", x2, y2, ",", x, y];
+    public cubic(x1: number, y1: number, x2: number, y2: number, x: number, y: number): PathDirective {
+        this.add(["C", x1, y1, ",", x2, y2, ",", x, y]);
+        return this;
     }
 
-    static cubicRelative(dx1: number, dy1: number, dx2: number, dy2: number, dx: number, dy: number) {
-        return ["c", dx1, dy1, ",", dx2, dy2, ",", dx, dy];
+    public cubicRelative(dx1: number, dy1: number, dx2: number, dy2: number, dx: number, dy: number): PathDirective {
+        this.add(["c", dx1, dy1, ",", dx2, dy2, ",", dx, dy]);
+        return this;
     }
 
-    static symmetricCubic(x2: number, y2: number, x: number, y: number) {
-        return ["S", x2, y2, ",", x, y];
+    public symmetricCubic(x2: number, y2: number, x: number, y: number): PathDirective {
+        this.add(["S", x2, y2, ",", x, y]);
+        return this;
     }
 
-    static symmetricCubicRelative(dx2: number, dy2: number, dx: number, dy: number) {
-        return ["s", dx2, dy2, ",", dx, dy];
+    public symmetricCubicRelative(dx2: number, dy2: number, dx: number, dy: number): PathDirective {
+        this.add(["s", dx2, dy2, ",", dx, dy]);
+        return this;
     }
     // #endregion Cubic Bezier
 
     // #region Quadratic Bezier
-    static quadratic(x1: number, y1: number, x: number, y: number) {
-        return ["Q", x1, y1, ",", x, y];
+    public quadratic(x1: number, y1: number, x: number, y: number): PathDirective {
+        this.add(["Q", x1, y1, ",", x, y]);
+        return this;
     }
 
-    static quadraticRelative(dx1: number, dy1: number, dx: number, dy: number) {
-        return ["q", dx1, dy1, ",", dx, dy];
+    public quadraticRelative(dx1: number, dy1: number, dx: number, dy: number): PathDirective {
+        this.add(["q", dx1, dy1, ",", dx, dy]);
+        return this;
     }
 
-    static symmetricQuadratic(x: number, y: number) {
-        return ["T", x, y];
+    public symmetricQuadratic(x: number, y: number): PathDirective {
+        this.add(["T", x, y]);
+        return this;
     }
 
-    static symmetricQuadraticRelative(dx: number, dy: number) {
-        return ["t", dx, dy];
+    public symmetricQuadraticRelative(dx: number, dy: number): PathDirective {
+        this.add(["t", dx, dy]);
+        return this;
     }
     // #endregion Quadratic Bezier
 
     // #region Arcs
-    static arc(rx: number, ry: number, xAxisRotation: number, largeArc: boolean, sweep: boolean, x: number, y: number) {
-        return ["A", rx, ry, xAxisRotation, largeArc ? 1 : 0, sweep ? 1 : 0, x, y];
+    public arc(rx: number, ry: number, xAxisRotation: number, largeArc: boolean, sweep: boolean, x: number, y: number): PathDirective {
+        this.add(["A", rx, ry, xAxisRotation, largeArc ? 1 : 0, sweep ? 1 : 0, x, y]);
+        return this;
     }
 
-    static arcRelative(rx: number, ry: number, xAxisRotation: number, largeArc: boolean, sweep: boolean, dx: number, dy: number) {
-        return ["a", rx, ry, xAxisRotation, largeArc ? 1 : 0, sweep ? 1 : 0, dx, dy];
+    public arcRelative(rx: number, ry: number, xAxisRotation: number, largeArc: boolean, sweep: boolean, dx: number, dy: number): PathDirective {
+        this.add(["a", rx, ry, xAxisRotation, largeArc ? 1 : 0, sweep ? 1 : 0, dx, dy]);
+        return this;
     }
 
     // #endregion Arcs
